80cb596d63d79122e1300a3429a8baea594a6e89
pydev <xtfkpi@gmail.com>
2016-03-17 13:25:26 +0200

    cards with design

--- generator.py
+++ generator.py
@@ -4,20 +4,21 @@ from jinja2 import Template
 
 
 def render_main(inv):
-    template = Template(open('trials.template.html', 'r', encoding='utf8').read())
+    template = Template(open('trials.investigations.template.html', 'r', encoding='utf8').read())
     f = open('index.html', 'w', encoding='utf8')
     f.write(template.render(inv=inv))
     f.close()
 
 
 def render_card(card):
-    template = Template(open('card.template.html', 'r', encoding='utf8').read())
+    template = Template(open('trials.card.template.html', 'r', encoding='utf8').read())
     f = open(card['file'], 'w', encoding='utf8')
     f.write(template.render(card=card))
     f.close()
 
 inv=pickle.load(open("2015.p", "rb"))
 render_main(inv)
+render_card(inv[0])
 # for i in inv:
 #     render_card(i)
 
